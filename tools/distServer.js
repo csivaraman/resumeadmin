daba4c996662bb7fd3f46402a0b7cbea725df50c
Chandrasekar Sivaraman <csivaraman1808@gmail.com>
2018-03-05 00:19:31 -0800

    modified dist servern

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,30 +5,22 @@ import compression from 'compression';
 
 /*eslint-disable no-console */
 
+const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  .get('*', (req, res) => res.render('../dist/index.html'))
-  .listen(port, () => console.log(`Listening on ${ port }`));
+app.use(compression());
+app.use(express.static('dist'));
 
-// app.use(compression());
-// app.use(express.static('dist'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, '../dist/index.html'));
-// });
-
-// app.listen(port, function(err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     open(`http://localhost:${port}`);
-
-//   }
-// });
-
-
+app.listen(port, host, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('server started');
+    // open(`:${port}`);
+  }
+});
