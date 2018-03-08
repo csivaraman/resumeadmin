98e6cb1252f42dd97e744644fb45bee67cf4ac0e
Chandrasekar Sivaraman <csivaraman1808@gmail.com>
2018-03-08 12:11:45 -0800

    upgreaded react and fixed dependencies

--- src/components/resume/ResumeForm.test.js
+++ src/components/resume/ResumeForm.test.js
@@ -1,7 +1,6 @@
 import expect from 'expect';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import TestUtils from 'react-addons-test-utils';
 import ResumeForm from './ResumeForm';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
